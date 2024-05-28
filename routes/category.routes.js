9c1db5f4e9206e1e11abe0102bbb3cbedcfb4f74
caseisjustin <132567763+caseisjustin@users.noreply.github.com>
2024-05-28 08:37:28 -0400

    Delete routes/category.routes.js

--- routes/category.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllcategories } from "../controller/category.controller.js";
-
-const router = Router()
-
-router.get("/", getAllcategories)
-
-export default router;
